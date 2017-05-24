24781a3ecd76eed6257f7946887d5394395018de
YANG Yulian <yangyulian202@gmail.com>
2017-05-24 13:24:34 +0800

    Update lstm_with_attention.py

--- lstm_with_attention.py
+++ lstm_with_attention.py
@@ -1,146 +1,64 @@
-""" LSTM-based click model
-    
-    The idea is to model the process of ad ranking as a sequence generating process. At each step, the model is used to predict the click through rate of the candidate ad accoding to its own quanlity and the ads selected at previous steps.
-
-    LSTM model is build using the open-sourced API keras. Keras is a high-level API based on tensorflow and and theano. It is a good tool for rapid prototyping 
-"""
-
-from keras.models import Sequential,load_model
-from keras.layers import Dense, Dropout
-from keras.layers import Embedding
-from keras.layers import LSTM
-from keras.layers import TimeDistributed
-from keras.layers import Masking
-import numpy as np
-from sklearn import metrics
-
-# feature dimention, time steps, ect
-data_dim = 26
-time_steps = 5
-num_classes = 2
-batchsize = 1000
-train_epoch = 15
-
-# calculate auc
-def AUC(label, pred):
-    pv_num = len(pred)
-    click_num = 0.0
-    area_num = 0.0
-    list_eval = zip(label, pred)
-    list_eval = sorted(list_eval, key = lambda x : x[1], reverse = True)
-    for i in range(len(list_eval)):
-        area_num += click_num * (1 - list_eval[i][0])
-        click_num += list_eval[i][0]
-    auc = 0.0
-    if pv_num == click_num:
-        auc = 1
-    elif click_num == 0:
-        auc = 0
-    else:
-        auc = area_num / (pv_num - click_num) / click_num
-    return (pv_num, click_num, auc)
+from keras import backend as K
+from keras.engine import InputSpec
+from keras.layers import LSTM, activations, Wrapper
 
 
-def load_data(mode, file_prefix):
-    # load training or testing data from files
-    if mode == "train":
-        x_train = np.load(file_prefix+'_feature.npy')
-        y_train = np.load(file_prefix+'_label.npy')
-        return (x_train, y_train)
-    elif mode == "test":
-        x_test = np.load(file_prefix+'_feature.npy')
-        y_test = np.load(file_prefix+'_label.npy')
-        return (x_test, y_test)
+class AttentionLSTM(LSTM):
+    def __init__(self, output_dim, attention_vec, attn_activation='tanh', single_attention_param=False, **kwargs):
+        self.attention_vec = attention_vec
+        self.attn_activation = activations.get(attn_activation)
+        self.single_attention_param = single_attention_param
 
-    # for run a toy model, one can generate ramdom data
-    # Generate dummy training data
-    #x_train = np.random.random((data_size, timesteps, data_dim))
-    #y_train = np.random.randint(2, size=(data_size, timesteps, 1))
+        super(AttentionLSTM, self).__init__(output_dim, **kwargs)
 
-    # Generate dummy validation data
-    #x_val = np.random.random((1, timesteps, data_dim))
-    #y_val = np.random.randint(1, size=(1, timesteps, 1))
+    def build(self, input_shape):
+        super(AttentionLSTM, self).build(input_shape)
 
-#####################################################################
-#### get context-free click through rate from feature vector   ######
-#### context-free click through rate is predicted by only considering 
-#### the information of user requirement and the single ad.    ######
-#####################################################################
-def get_ctr(x_test):
-    ctr = []
-    for i in range(0, len(x_test)):
-        for j in range(0, time_steps):
-            ctr.append(round(x_test[i][j][0],time_steps))
+        if hasattr(self.attention_vec, '_keras_shape'):
+            attention_dim = self.attention_vec._keras_shape[1]
+        else:
+            raise Exception('Layer could not be build: No information about expected input shape.')
 
-    return np.asarray(ctr)
+        self.U_a = self.inner_init((self.output_dim, self.output_dim),
+                                   name='{}_U_a'.format(self.name))
+        self.b_a = K.zeros((self.output_dim,), name='{}_b_a'.format(self.name))
 
-def build_model():
-    model = Sequential()
-    model.add(LSTM(32, return_sequences=True, input_shape=(timesteps, data_dim)))
-    # output at each time step
-    model.add(TimeDistributed(Dense(1,activation='sigmoid')))
-    model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-    return model
+        self.U_m = self.inner_init((attention_dim, self.output_dim),
+                                   name='{}_U_m'.format(self.name))
+        self.b_m = K.zeros((self.output_dim,), name='{}_b_m'.format(self.name))
 
+        if self.single_attention_param:
+            self.U_s = self.inner_init((self.output_dim, 1),
+                                       name='{}_U_s'.format(self.name))
+            self.b_s = K.zeros((1,), name='{}_b_s'.format(self.name))
+        else:
+            self.U_s = self.inner_init((self.output_dim, self.output_dim),
+                                       name='{}_U_s'.format(self.name))
+            self.b_s = K.zeros((self.output_dim,), name='{}_b_s'.format(self.name))
 
-def train_model(model):
-    file_prefix_list = []
-    
-    ###########################################
-    ##  code for read the training file list ##
-    ###########################################
+        self.trainable_weights += [self.U_a, self.U_m, self.U_s, self.b_a, self.b_m, self.b_s]
 
-    for j in range(0, train_epoch):
-        # train the model with data from all the training file
-        for i in range(0, len(file_prefix_list)):
-            (x_train, y_train) = load_data("train", file_prefix_list[i])
-            print "data loaded: ", len(x_train), len(y_train)
-            model.fit(x_train, y_train, nb_epoch=1, batch_size=batchsize)
-            del x_train, y_train
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
 
+    def step(self, x, states):
+        h, [h, c] = super(AttentionLSTM, self).step(x, states)
+        attention = states[4]
 
-def save_model(model):
-    model.summary()
-    model.save("./lstm_model")
-    model.save_weights('./lstm_model_weights')
+        m = self.attn_activation(K.dot(h, self.U_a) * attention + self.b_a)
+        # Intuitively it makes more sense to use a sigmoid (was getting some NaN problems
+        # which I think might have been caused by the exponential function -> gradients blow up)
+        s = K.sigmoid(K.dot(m, self.U_s) + self.b_s)
 
-def evaluate_model(model):
-    test_file = ""
+        if self.single_attention_param:
+            h = h * K.repeat_elements(s, self.output_dim, axis=1)
+        else:
+            h = h * s
 
-    ############################################
-    ##       code for initiate test_file      ##
-    ############################################
+        return h, [h, c]
 
-    (x_test, y_test) = load_data("test", test_file)
-
-    preds = model.predict(x_test, batch_size=batchsize, verbose=0)
-
-    ctr_base = get_ctr(x_test) 
-
-    # evaluate with auc
-    print "New AUC:"
-    print AUC(y_test, preds)
-    print "Old AUC:"
-    print AUC(y_test, ctr_base)    
-
-def main():
-    model = build_model()
-    print "model built!"
-
-    train_model(model)
-    print "model trained!"
-
-    save_model(model)
-    print "model saved!"
-    del model    
-
-    model = load_model("./lstm_model")
-    model.load_weights('./lstm_model_weights', by_name=True)
-    model.summary()
-    # model evaluation
-    evaluate_model(model)
-
-if __name__ == "__main__":    
-    main()
+    def get_constants(self, x):
+        constants = super(AttentionLSTM, self).get_constants(x)
+        constants.append(K.dot(self.attention_vec, self.U_m) + self.b_m)
+        return constants
